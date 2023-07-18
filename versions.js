6b8cd06ed90613cd20bc974e743488b061ddd26a
Zachh2 <126375621+Zachh2@users.noreply.github.com>
2023-07-19 01:21:23 +0800

    Create versions.js

---
+++ versions.js
@@ -0,0 +1,14 @@
+[
+  	{
+		"version": "1.0.1",
+		"info": {
+			"scripts/cmds/banner.js": "fix error when call api",
+			"scripts/cmds/banner2.js": "fix error when call api",
+			"scripts/cmds/bannerbw.js": "fix error when call api",
+			"scripts/cmds/cmd.js": "synchronized envGlobal & envConfig when load scripts",
+			"scripts/cmds/adminonly.js": "new script command",
+			"bot/loadAllScript.js": "synchronized envGlobal & envConfig when load scripts",
+			"index.js": "Fix error can't use adminOnly"
+		}
+	}
+  ]
